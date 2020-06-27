079255eeda7b21993f693ef8109421d03cca9b7f
huangt <1416501312@qq.com>
2020-06-27 19:01:48 +0800

    11

--- src/api/loginapi.js
+++ src/api/loginapi.js
@@ -12,7 +12,28 @@ export const authLoginByAccount = (data) => {
 
 export const getTasks = (data) => {
   return request({
-    url: `/api/api/tasks/?page=${data.page}&page_size=${data.pageSize}&type=${data.type}`,
+    url: `/api/api/tasks/?page=${data.page}&page_size=${data.pageSize}&type=${data.type}&state=1`,
+    method: 'get'
+  })
+}
+
+export const getNews = (data) => {
+  return request({
+    url: `/api/api/news/?page=${data.page}&page_size=${data.pageSize}`,
+    method: 'get'
+  })
+}
+
+export const getNewsDetails = (data) => {
+  return request({
+    url: `/api/api/news/${data.id}/`,
+    method: 'get'
+  })
+}
+
+export const getTasksSelfComplete = (data) => {
+  return request({
+    url: `/api/api/complete_tasks/?page=${data.page}&page_size=${data.pageSize}&state=${data.state}`,
     method: 'get'
   })
 }
--- src/views/home/news/newsDetail.vue
+++ src/views/home/news/newsDetail.vue
@@ -2,19 +2,39 @@
   <van-cell-group
     style="margin-top: 44px; background: #FFFFFF; height: 100%;">
     <div style="text-align: center; font-size: 22px; font-weight: 700; padding: 10px;">
-      端午节特别活动
+      {{ this.new.new_title }}
     </div>
     <van-divider />
-    <div style="padding: 10px;">&emsp;&emsp;端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动端午节特别活动</div>
+    <div style="padding: 10px;">&emsp;&emsp;{{ this.new.new_content }}</div>
   </van-cell-group>
 </template>
 
 <script>
 import { Cell, CellGroup, Divider } from 'vant'
+import { getNewsDetails } from '@/api/loginapi'
 export default {
     name: 'newsDetail',
+    data () {
+      return {
+        new: {}
+      }
+    },
     created () {
       console.log(this.$route.params.id)
+      this.page++
+      let data = {
+        id: this.$route.params.id
+      }
+      getNewsDetails(data).then(res => {
+        // if (this.taskList.length === 0) {
+        //   this.taskList = res.data.results
+        // } else {
+        //   this.taskList.push(res.data.results)
+        // }
+        // this.taskList.push(res.data.results)
+        // this.taskList = JSON.parse(JSON.stringify(this.taskList))
+        this.new = res.data
+      })
     },
   components: {
     [Cell.name]: Cell,
