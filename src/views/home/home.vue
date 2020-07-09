ca3312581b8c3e155342acbae1723f5fbb1b9685
huangt <1416501312@qq.com>
2020-07-09 20:20:54 +0800

    11

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -9,7 +9,8 @@
       <van-swipe-item v-for="(banner, index) in bannerList"
                       :key="index">
 <!--        <div class="bg" :style="{background-image: url(' + banner.image + ') no-repeat}"></div>-->
-        <div class="bg" :style="{ 'background-image': 'url(' + banner.image.replace('127.0.0.1', '39.108.145.250') + ')'}">
+<!--        <div class="bg" :style="{ 'background-image': 'url(' + banner.image.replace('127.0.0.1', '39.108.145.250') + ')'}">-->
+        <div class="bg" :style="{ 'background-image': 'url(' + banner.image.replace('39.108.145.250', '39.108.145.250:8080') + ')'}">
         </div>
 <!--        <img :src="banner.image"-->
 <!--             style="height:230px">-->
